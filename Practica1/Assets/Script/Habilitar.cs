4351ebbd9916755bdbcb67f1a832ebf116da3258
Diego Cruz <abeja.dact_@hotmail.com>
2017-04-27 19:02:52 -0500

    Commit Final Clase 4

---
+++ Practica1/Assets/Script/Habilitar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Habilitar : MonoBehaviour {
+
+	public GameObject objetoHabilitar=null;
+
+	// Use this for initialization
+	void Start () {
+		objetoHabilitar.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Practica1/Assets/Script/ScriptTarde.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTarde : MonoBehaviour {
+	public int numeroVeces=0;
+	public GameObject objeto=null;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World");
+		Debug.Log("Numero Veces: "+numeroVeces );
+		Debug.Log ("Mi Nombre: " + this.gameObject.name);
+		Debug.Log ("Nombre Objeto: "+objeto.name);
+
+		Vector3 posicion=  new Vector3();
+		posicion = this.gameObject.transform.position;
+		//Mostrar Los Transform de cada Objeto
+		Debug.Log("Mi posicion: "+posicion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
