22b9373aff3b8aab1cf359b521bce72459963d5c
Diego Cruz <abeja.dact_@hotmail.com>
2017-05-02 19:06:42 -0500

    Ultimo Commit

---
+++ Practica1/Assets/Script/Click.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject Esfera = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			Debug.Log ("Pressed right click.");
+			Esfera.transform.position = new Vector3(2, 2, 0);
+		}
+	}
+}
