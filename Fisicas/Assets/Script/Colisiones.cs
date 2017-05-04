14cb5ce26efe567bab9ed8ef129ee8a8e89da3ed
Diego Cruz <abeja.dact_@hotmail.com>
2017-05-04 17:02:38 -0500

    Commit Fisicas

---
+++ Fisicas/Assets/Script/Colisiones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisiones : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision miColision)
+	{
+		Debug.Log("Colision con " + miColision.gameObject.name);
+	}
+
+	void OnCollisionExit(Collision miColision)
+	{
+		Debug.Log("Salgo con " + miColision.gameObject.name);
+	}
+
+	void OnCollisionStay(Collision miColision)
+	{
+		Debug.Log("Estoy con " + miColision.gameObject.name);
+	}
+
+	void OnTriggerStay(Collider miColision)
+	{
+		Debug.Log("Atravieso " + miColision.gameObject.name);
+	}
+}
---
+++ Fisicas/Assets/Script/Fuerzas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuerzas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * 20f, ForceMode.Impulse);
+		//this.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * 20f, ForceMode.VelocityChange);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Tiempo Update:" + Time.deltaTime);
+	}
+	void FixedUpdate(){
+		//this.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * 20f, ForceMode.Force);
+		Debug.Log ("Tiempo Fixeupdate:" + Time.deltaTime);
+	}
+}
