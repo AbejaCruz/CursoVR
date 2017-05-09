b9c5eff038f425a2ce002097dbddc289f71d83e5
Diego Cruz <abeja.dact_@hotmail.com>
2017-05-08 22:25:56 -0500

    Commit clase 8

---
+++ BombARdeo/Assets/Script/Disparo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+	public GameObject Objeto;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("Pressed left click.");
+			GameObject.Instantiate (Objeto, new Vector3 (2, 3, 3), Objeto.transform.rotation);
+
+		}
+	}
+}
